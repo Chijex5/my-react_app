3a5775e223c178090f25b3be4baad1f3b12ff77f
DAniel <asogwadanny@gmail.com>
2024-06-27 13:38:05 +0100

    chijioke

--- src/components/NotifyMeButton.js
+++ src/components/NotifyMeButton.js
@@ -1,9 +1,7 @@
 import React from 'react';
-
+import axios from 'axios';
 import './NotifyMeButton.css';
 const BASE_URL = "https://mybackend-2.onrender.com";
-// Instead of import axios from 'axios';
-const axios = require('axios');
 
 
 const NotifyMeButton = ({ book, handleNotifyMe }) => {
